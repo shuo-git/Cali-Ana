891c0354f370b3c099e505853cdac5ef9fc88b1f
shuo-git <315665262@qq.com>
2020-06-28 10:32:14 +0800

    update

---
+++ extract.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+CODE=./
+extract(){
+	GEN=$1
+	grep ^T $GEN | python3 $CODE/sorted_cut_fairseq_gen.py 1 > $GEN.ref
+	grep ^H $GEN | python3 $CODE/sorted_cut_fairseq_gen.py 2 > $GEN.sys
+	grep ^P $GEN | python3 $CODE/sorted_cut_fairseq_gen.py 1 > $GEN.prob
+}
+
+DIR="/apdcephfs/share_916081/vinceswang/results/wmt14_en_de_stanford_ada_cali_base-bak/inference"
+for step in {2000..100000..2000};do
+	for SUBSET in valid test;do
+		extract $DIR/${SUBSET}_${step}.gen
+	done
+done
---
+++ sorted_cut_fairseq_gen.py
@@ -0,0 +1,13 @@
+import sys
+
+idx = int(sys.argv[1])
+lines=[]
+for line in sys.stdin:
+	line=line.strip().split('\t')
+	num=int(line[0].split('-')[1])
+	sent=line[idx]
+	lines.append((num, sent))
+
+lines=sorted(lines, key=lambda x:x[0])
+for line in lines:
+	print(line[1])
