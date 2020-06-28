c560f3784eaffebd4d42f37bbbb0512414b3c7a0
shuo-git <315665262@qq.com>
2020-06-28 14:17:07 +0800

    update

--- extract.sh
+++ extract.sh
@@ -1,15 +1,20 @@
 # !/bin/bash
 CODE=./
-extract(){
+extract_gen(){
 	GEN=$1
 	grep ^T $GEN | python3 $CODE/sorted_cut_fairseq_gen.py 1 > $GEN.ref
 	grep ^H $GEN | python3 $CODE/sorted_cut_fairseq_gen.py 2 > $GEN.sys
 	grep ^P $GEN | python3 $CODE/sorted_cut_fairseq_gen.py 1 > $GEN.prob
 }
 
-DIR="/apdcephfs/share_916081/vinceswang/results/wmt14_en_de_stanford_base/inference"
-for step in {2000..100000..2000};do
-	for SUBSET in valid test;do
-		extract $DIR/${SUBSET}_${step}.gen
+extract_fd(){
+	FD=$1
+	python3 $CODE/extract_force_dec.py $FD
+}
+
+DIR="/apdcephfs/share_916081/vinceswang/results/wmt14_en_de_stanford_base/score/sample_status"
+for step in 2000;do
+	for SUBSET in valid;do
+		extract_fd $DIR/status_${SUBSET}_${step}.txt
 	done
 done
---
+++ extract_force_dec.py
@@ -0,0 +1,24 @@
+import sys
+from utils import *
+
+
+def delete_pad(_list):
+    res_list = []
+    for item in _list:
+        if item == '2.0':
+            break
+        res_list.append(item)
+    return res_list
+
+
+lines = file2lines(sys.argv[1])
+lines = [l.split('|||') for l in lines]
+indices = [int(l[0]) for l in lines]
+probs = [delete_pad(l[1].split()) for l in lines]
+accs = [delete_pad(l[2].split()) for l in lines]
+data = zip(indices, probs, accs)
+sort(data, key=lambda x: x[0])
+indices, probs, accs = zip(*data)
+
+words2file(probs, sys.argv[1] + '.prob')
+words2file(acc, sys.argv[1] + '.acc')
