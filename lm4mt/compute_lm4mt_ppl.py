e4fe616112f4c720fb170be9a4c4b45d53eec611
shuo-git <315665262@qq.com>
2021-03-09 14:30:27 +0800

    update 52

--- lm4mt/compute_lm4mt_ppl.py
+++ lm4mt/compute_lm4mt_ppl.py
@@ -6,8 +6,14 @@ from utils import *
 
 with open(sys.argv[1], 'r') as fr:
     lines = fr.readlines()
+with open(sys.argv[2], 'r') as fr:
+    src_lines = fr.readlines()
 
-lines = [[float(x) for x in l.split()] for l in lines]
+lines = [l.split() for l in lines]
+src_lengths = [len(l.split()) for l in src_lines]
+lines = [l[i+2:] for l, i in zip(lines, src_lengths)]
+
+lines = [[float(x) for x in l] for l in lines]
 
 count = sum([len(l) for l in lines])
 
